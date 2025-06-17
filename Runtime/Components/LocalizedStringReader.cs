fdfbe30d11229c1718133e17c509829409dbfcd0
Boleslav Horak <bhorak@the-mark.com>
2025-06-17 16:39:19 +0200

    LocalizedString now IEquatable. LocalizedStringReader.LoclaizedString can be set from code.

--- Runtime/Components/LocalizedStringReader.cs
+++ Runtime/Components/LocalizedStringReader.cs
@@ -1,34 +1,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
+
 namespace SeweralIdeas.Localization
 {
     public class LocalizedStringReader : MonoBehaviour
     {
-        [SerializeField] private LocalizedString m_localizedString;
-        [SerializeField] private UnityEvent<string> m_updateEvent = new();
+        [SerializeField] 
+        [FormerlySerializedAs("m_localizedString")] 
+        private LocalizedString _localizedString;
         
+        [SerializeField] 
+        [FormerlySerializedAs("m_updateEvent")] 
+        private UnityEvent<string> _updateEvent = new();
+
+        public LocalizedString LocalizedString
+        {
+            get => _localizedString;
+            set
+            {
+                if (_localizedString == value)
+                    return;
+                _localizedString = value;
+                UpdateText();
+            }
+        }
+
         private void OnEnable() => GlobalLanguage.Language.Changed += OnLanguageLoaded;
 
         private void OnDisable() => GlobalLanguage.Language.Changed -= OnLanguageLoaded;
 
         private void OnLanguageLoaded(LanguageData languageData, LanguageData oldData) => UpdateText();
 
-        private void UpdateText() => m_updateEvent.Invoke(GetText(GlobalLanguage.Language.Value));
+        private void UpdateText() => _updateEvent.Invoke(GetText(GlobalLanguage.Language.Value));
 
         private string GetText(LanguageData languageData)
         {
             if(languageData == null)
                 return "NO LANGUAGE";
-            var text = languageData.Texts.GetValueOrDefault(m_localizedString.Key, "NOT FOUND");
+            var text = languageData.Texts.GetValueOrDefault(_localizedString.Key ?? string.Empty, "NOT FOUND");
             return text;
         }
 
         #if UNITY_EDITOR
         protected void OnValidate()
         {
-            if(Application.isPlaying && gameObject.scene.IsValid())
-                UpdateText();
+            UpdateText();
         }
         #endif
     }
--- Runtime/LocalizedString.cs
+++ Runtime/LocalizedString.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 namespace SeweralIdeas.Localization
 {
     [Serializable]
-    public struct LocalizedString
+    public struct LocalizedString : IEquatable<LocalizedString>
     {
         [SerializeField] private string m_key;
         public string Key => m_key;
+        
         public string Text
         {
             get
@@ -20,5 +21,16 @@ namespace SeweralIdeas.Localization
 
             }
         }
+        
+        
+        public bool Equals(LocalizedString other) => m_key == other.m_key;
+
+        public override bool Equals(object obj) => obj is LocalizedString other && Equals(other);
+
+        public override int GetHashCode() => (m_key != null ? m_key.GetHashCode() : 0);
+
+        public static bool operator ==(LocalizedString left, LocalizedString right) => left.Equals(right);
+
+        public static bool operator !=(LocalizedString left, LocalizedString right) => !left.Equals(right);
     }
 }
