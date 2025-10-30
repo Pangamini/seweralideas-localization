90efddd3784ee6a850b84a52274a950439c6bdec
Boleslav Horak <bhorak@the-mark.com>
2025-10-30 12:18:43 +0100

    LocalizedStringReader value can now be null

--- Runtime/Components/LocalizedStringReader.cs
+++ Runtime/Components/LocalizedStringReader.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using SeweralIdeas.UnityUtils.Drawers;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Serialization;
@@ -7,7 +8,11 @@ namespace SeweralIdeas.Localization
 {
     public class LocalizedStringReader : MonoBehaviour
     {
+        [SerializeField]
+        private bool _hasValue = true;
+        
         [SerializeField] 
+        [Condition(nameof(_hasValue))]
         [FormerlySerializedAs("m_localizedString")] 
         private LocalizedString _localizedString;
         
@@ -15,14 +20,27 @@ namespace SeweralIdeas.Localization
         [FormerlySerializedAs("m_updateEvent")] 
         private UnityEvent<string> _updateEvent = new();
 
-        public LocalizedString LocalizedString
+        public LocalizedString? LocalizedString
         {
-            get => _localizedString;
+            get => _hasValue ? _localizedString : null; 
             set
             {
-                if (_localizedString == value)
+                if (!_hasValue && !value.HasValue)
                     return;
-                _localizedString = value;
+                
+                if ( value is {} newValue)
+                {
+                    if(_hasValue && _localizedString == newValue) 
+                        return;
+                    
+                    _localizedString = newValue;
+                    _hasValue = true;
+                }
+                else
+                {
+                    _hasValue = false;
+                }
+
                 UpdateText();
             }
         }
